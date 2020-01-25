b23bc44c565c6215655fbc894c56e48a26f90263
dlwagner <wagner.don@gmail.com>
2020-01-24 21:22:10 -0500

    ready for prod

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  // Import routes for "catalog" area of site.
 
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 //var mongoDB = dbUrl;
 var mongoDB = process.env.MONGODB_URI;
-console.log("mongoDB: " + mongoDB);
+//console.log("mongoDB: " + mongoDB);
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -28,6 +30,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
